cd382b13ce6f43b3e383ef5a2954b02ee7011344
JesperLH <JesperLH@users.noreply.github.com>
2020-01-22 14:14:03 +0100

    Drop delayed modelling of tau/lambda if below convergence crit.

--- myclasses/helperfunctions/isElboDiverging.m
+++ myclasses/helperfunctions/isElboDiverging.m
@@ -10,6 +10,22 @@ for i = 1:nargin-3
     varargout{i} = inf; %#ok<AGROW>
 end
 
+ if nargin > 3 && delta_cost < conv_crit % Variable inputs
+    % If the relative change is below convergence, but tau/lambda is not
+    % yet modelled.
+    for i = 1:nargin-3
+        if varargin{i}{2} && current_iteration < varargin{i}{3}
+            sw = sprintf('%s Starting to model %s now (iter. %i).',...
+                            '', varargin{i}{1},current_iteration);
+            varargout{i} = current_iteration;
+            
+            warning(sw)
+            break
+        end
+
+    end
+end
+
 if isDiverging && abs(delta_cost) < conv_crit
     sw = sprintf('Soft-error: Lowerbound diverged in iteration %i, by %6.4e (relative err.).',...
                 current_iteration,delta_cost);
