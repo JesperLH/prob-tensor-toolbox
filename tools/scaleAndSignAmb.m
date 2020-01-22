84771b1711936b61a1a6e394d7e2cee09f8fa958
JesperLH <JesperLH@users.noreply.github.com>
2020-01-22 14:15:43 +0100

    Update scaleAndSignAmb.m

--- tools/scaleAndSignAmb.m
+++ tools/scaleAndSignAmb.m
@@ -1,18 +1,18 @@
-function A = scaleAndSignAmb(A);
+function [A,scale] = scaleAndSignAmb(A)
 
 scale = ones(1,size(A{1},2));
 
-for n = 2:length(A);
-    scale = scale.*sqrt(sum(A{n}.^2, 1));
-    A{n} = bsxfun(@rdivide,A{n},sqrt(sum(A{n}.^2, 1)));
+for n = 2:length(A)
+    scale = scale.*sqrt(mean(A{n}.^2, 1));
+    A{n} = bsxfun(@rdivide,A{n},sqrt(mean(A{n}.^2, 1)));
 end
 A{1} = bsxfun(@times,A{1},scale);
 
 
-scale = ones(1,size(A{1},2));
+scale_sign = ones(1,size(A{1},2));
 
-for n = 2:length(A);
-    scale = scale.*sign(sum(A{n}, 1));
+for n = 2:length(A)
+    scale_sign = scale_sign.*sign(sum(A{n}, 1));
     A{n} = bsxfun(@rdivide,A{n},sign(sum(A{n}, 1)));
 end
-A{1} = bsxfun(@times,A{1},scale);
+A{1} = bsxfun(@times,A{1},scale_sign);
