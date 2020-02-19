8e75dfb1585407fcd405cf3b91361264071d4741
JesperLH <JesperLH@users.noreply.github.com>
2020-02-19 14:25:02 +0100

    Update setup_paths.m

--- setup_paths.m
+++ setup_paths.m
@@ -2,6 +2,6 @@
 addpath(genpath('./tools'))
 addpath('./myMatlab')
 addpath(genpath('./myclasses/'))
-
+addpath(genpath('./prob-tt/'));
 addpath('../thirdparty-matlab/nway331/') % Download from http://www.models.life.ku.dk/nwaytoolbox
 
