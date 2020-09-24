a4a9d2e5274fe45ba4e44c3f82659982945201fe
JesperLH <JesperLH@users.noreply.github.com>
2020-09-24 15:35:01 +0200

    Update db_diff.m

--- tools/utils/db_diff.m
+++ tools/utils/db_diff.m
@@ -2,8 +2,8 @@
 %
 %function [abs_diff, rel_diff] = db_diff(A,B,str_name)
 function db_diff(A,B,str_name)
-    abs_diff = norm(A(:)-B(:))^2;
-    rel_diff = abs_diff/norm(A(:))^2;
+    abs_diff = norm(A(:)-B(:));
+    rel_diff = abs_diff/norm(A(:));
     fprintf('%12s:\tabs_diff: %4.6e\trel_diff: %4.6e\n',str_name,abs_diff,rel_diff);
     if rel_diff > 1e-16
 %         fprintf('A large difference was observed, do you which to continue? (press F5)\n')
