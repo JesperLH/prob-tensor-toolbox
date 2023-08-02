8d20dc39151d5be2f14b7394646115d790f0d407
JesperLH <JesperLH@users.noreply.github.com>
2023-08-02 10:13:16 +0200

    Allow matricizing to be specified for multiple modes at once

--- tools/tensor/algebra/matricizing.m
+++ tools/tensor/algebra/matricizing.m
@@ -13,4 +13,13 @@ function Y=matricizing(X,n)
 % Y     The corresponding matriziced version of X
 
 N=ndims(X);
-Y=reshape(permute(X, [n 1:n-1 n+1:N]),size(X,n),numel(X)/size(X,n));
+% Y=reshape(permute(X, [n 1:n-1 n+1:N]),size(X,n),numel(X)/size(X,n));
+if isempty(n)
+    
+elseif n == N+1
+    Y = X(:).';
+else
+    sX = size(X);
+    n2 = setdiff(1:N,n);
+    Y = reshape(permute(X, [n n2]),prod(sX(n)),prod(sX(n2)));
+end
