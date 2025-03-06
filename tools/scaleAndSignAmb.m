1f2f161773211392e68806a6bdc21449a675ab6f
JesperLH <JesperLH@users.noreply.github.com>
2025-03-06 10:03:02 +0100

    Specify mode on which to transfer the scale of each component

--- tools/scaleAndSignAmb.m
+++ tools/scaleAndSignAmb.m
@@ -1,18 +1,27 @@
-function [A,scale] = scaleAndSignAmb(A)
+function [A,scale] = scaleAndSignAmb(A,idx_scale)
+
+if nargin < 2
+    idx_scale = 1;
+end
 
 scale = ones(1,size(A{1},2));
 
-for n = 2:length(A)
-    scale = scale.*sqrt(var(A{n},[],1));%mean(A{n}.^2, 1));
-    A{n} = bsxfun(@rdivide,A{n},sqrt(var(A{n},[],1)));%mean(A{n}.^2, 1)));
+for n = setdiff(1:length(A),idx_scale)
+    % scale = scale.*sqrt(var(A{n},[],1));%mean(A{n}.^2, 1));
+    this_scale = sqrt(sum(A{n}.^2,1));
+    % this_scale = abs(mean(A{n},1));
+    % this_scale = sum(A{n},1);
+    % this_scale = max(A{n},[],1);
+    scale = scale.*this_scale;
+    A{n} = bsxfun(@rdivide,A{n},this_scale);%mean(A{n}.^2, 1)));
 end
-A{1} = bsxfun(@times,A{1},scale);
+A{idx_scale} = bsxfun(@times,A{idx_scale},scale);
 
 
 scale_sign = ones(1,size(A{1},2));
 
-for n = 2:length(A)
+for n = setdiff(1:length(A),idx_scale)
     scale_sign = scale_sign.*sign(sum(A{n}, 1));
     A{n} = bsxfun(@rdivide,A{n},sign(sum(A{n}, 1)));
 end
-A{1} = bsxfun(@times,A{1},scale_sign);
+A{idx_scale} = bsxfun(@times,A{idx_scale},scale_sign);
